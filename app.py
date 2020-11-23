68becb292ec289d9116fb7d0120372a99a4c8659
asteennilsen@gmail.com <asteennilsen@gmail.com>
2020-11-23 13:19:42 +0100

    added all ns and random order

--- app.py
+++ app.py
@@ -17,7 +17,10 @@ def hello_world():
         line_ragna_ns = ['172468', '172469', '172477', '172898', '173272']
         anders_ns = ['173393', '173394', '173446', '173511', '173517']
         siva_ns = ['173516', '173733', '173696', '173682', '173764']
-        nettskjema_ids = line_ragna_ns + anders_ns + siva_ns
+        gorel_ns = ['174368', '174371', '174380', '174390', '174392']
+        annika_ns = ['174437', '174439', '174441', '174442']
+        #nettskjema_ids = line_ragna_ns + anders_ns + siva_ns + gorel_ns + annika_ns
+        nettskjema_ids = ['174371','173764', '173733', '174380', '173696', '174439', '173517', '173446', '172469', '173516', '174392', '173682', '174437', '173511', '172468', '174390', '173272', '173394', '173393', '172898', '174442', '172477', '174368', '174441']
         if 'clicked_date' in session:
             clicked_dict = session['clicked_date']
         return render_template('calendar.html', username = session['username'], current_day=current_day, clicked_dict = clicked_dict, nettskjema_ids=nettskjema_ids)
